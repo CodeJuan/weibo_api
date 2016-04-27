8c46af77ef72b81f2411bdb4756e34f1453f2e58
codejuan <xh@decbug.com>
2016-04-27 23:54:43 +0800

    haha

--- test.go
+++ test.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "fmt"
     "github.com/huichen/gobo"
+    "strconv"
 )
 
 var (
@@ -15,17 +16,43 @@ func main() {
     // 解析命令行参数
     flag.Parse()
 
+    //post
+    postWeibo()
+
     // 调用API
     var statuses gobo.Statuses
-    params := gobo.Params{"screen_name": "人民日报", "count": 10}
-    err := weibo.Call("statuses/user_timeline", "get", *access_token, params, &statuses)
+    params := gobo.Params{"sort": 1, "lat":34.19022, "long":108.84218, "range":11132}
+    err := weibo.Call("place/nearby_timeline", "get", *access_token, params, &statuses)
+
+//    params := gobo.Params{"poiid": "B2094656D269A6FA419F"}
+//    err := weibo.Call("place/poi_timeline", "get", *access_token, params, &statuses)
 
     // 处理返回结果
     if err != nil {
         fmt.Println(err)
         return
     }
+    fmt.Println(statuses.Statuses)
     for _, status := range statuses.Statuses {
         fmt.Println(status.Text)
+        id, _ := strconv.ParseInt(status.Id, 10, 64)
+        comments := gobo.Params{"id": id, "comment": "测试一下，马上删除"	}
+        err := weibo.Call("comments/create", "post", *access_token, comments, nil)
+        if err != nil {
+            fmt.Println(err)
+        }
     }
+}
+
+
+func postWeibo(){
+//    var statuses gobo.Statuses
+//    params := gobo.Params{"status": "test2", "lat":34.19394, "long":108.84218}
+//    err := weibo.Call("statuses/update", "post", *access_token, params, &statuses)
+//
+//    // 处理返回结果
+//    if err != nil {
+//        fmt.Println(err)
+//        return
+//    }
 }
