690f6eabf4c616f8a988d8443f7e32907f2d41ed
codejuan <xh@decbug.com>
2016-04-28 00:16:26 +0800

    like

--- test.go
+++ test.go
@@ -21,7 +21,7 @@ func main() {
 
     // 调用API
     var statuses gobo.Statuses
-    params := gobo.Params{"sort": 1, "lat":34.19022, "long":108.84218, "range":11132}
+    params := gobo.Params{"sort": 0, "lat":34.19394, "long":108.84218, "range":11132}
     err := weibo.Call("place/nearby_timeline", "get", *access_token, params, &statuses)
 
 //    params := gobo.Params{"poiid": "B2094656D269A6FA419F"}
@@ -34,10 +34,10 @@ func main() {
     }
     fmt.Println(statuses.Statuses)
     for _, status := range statuses.Statuses {
-        fmt.Println(status.Text)
+        fmt.Println(status)
         id, _ := strconv.ParseInt(status.Id, 10, 64)
-        comments := gobo.Params{"id": id, "comment": "测试一下，马上删除"	}
-        err := weibo.Call("comments/create", "post", *access_token, comments, nil)
+        comments := gobo.Params{"id": id, "attitude": "smile"}
+        err := weibo.Call("attitudes/create", "post", *access_token, comments, nil)
         if err != nil {
             fmt.Println(err)
         }
